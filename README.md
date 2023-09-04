d6dc939110fdeff346da0d81e63990ed4fec7ed2
Alessio Caputo <84250128+alessioC42@users.noreply.github.com>
2023-09-04 17:50:28 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+
+## Build
+```bash
+$ androidjs b
+```
+
+working.
---
+++ SPH-Plan/main.js
@@ -0,0 +1,35 @@
+const back = require('androidjs').back;
+const { SPHclient } = require('sphclient');
+
+var sphclient = new SPHclient();
+
+
+back.on("sphclient.auth", async (username, pass, schoolID) => {
+	sphclient = new SPHclient(username, pass, schoolID);
+	try {
+		await sphclient.authenticate();
+		back.send("loginSuccessful", true);
+	} catch (_err) {
+		back.send("loginSuccessful", false);
+	}
+});
+
+back.on("sphclient.isAuth", async () => {
+	try {
+		await sphclient.getVplan(new Date());
+		back.send("sphclient.isAuth", true);
+	} catch {
+		back.send("sphclient.isAuth", false);
+	}
+});
+
+back.on("sphclient.getVplan", async () => {
+	try {
+		back.send(
+			"sphclient.Vplan", 
+			await sphclient.getVplan(await sphclient.getNextVplanDate())
+		);
+	} catch {
+		back.send("sphclient.isAuth", false);
+	}
+});
