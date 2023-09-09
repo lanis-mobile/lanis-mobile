6bda09c26e915d2dc2019c7fe007d938cd9e51cf
Alessio Caputo <84250128+alessioC42@users.noreply.github.com>
2023-09-09 19:46:55 +0200

    UI is hard

--- app/src/script.js
+++ app/src/script.js
@@ -1,6 +1,33 @@
-try {
-    var app = new Framework7();
+// Initialize app
+var app = new Framework7({
+    // App root element
+    root: '#app',
+    // App Name
+    name: 'My App',
+    // App id
+    id: 'com.myapp.test',
+    // Enable swipe panel
+    panel: {
+        swipe: 'left',
+    },
+});
 
-} catch (err) {
-    alert(err)
+function openSettingsScreen() {
+    app.loginScreen.open('#settings-screen');
 }
+
+function closeSettingsScreen() {
+    app.loginScreen.close('#settings-screen');
+}
+
+document.getElementById("openSettingsScreenButton").addEventListener("click", () => {
+    openSettingsScreen()
+})
+
+app.tab.show('#instanceConfigTab');
+
+  // Event-Handling für das Umschalten zwischen Tabs
+  app.on('tabShow', function (tabEl) {
+    var tabId = tabEl.id;
+    app.tab.show(tabId);
+  });
