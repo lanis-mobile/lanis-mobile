c73dbd81040432756014bd290f6cf5a3504a3482
Alessio Caputo <84250128+alessioC42@users.noreply.github.com>
2023-09-10 09:28:36 +0200

    add school search

--- app/src/script.js
+++ app/src/script.js
@@ -1,3 +1,7 @@
+import Fuse from 'fuse.js';
+import schoolData from './schools.json';
+
+
 // Initialize app
 var app = new Framework7({
     // App root element
@@ -12,12 +16,45 @@ var app = new Framework7({
     },
 });
 
+
+
 function openSettingsScreen() {
     app.loginScreen.open('#settings-screen');
+    loadSchoolSelect();
 }
 
 function closeSettingsScreen() {
     app.loginScreen.close('#settings-screen');
+}
+
+
+var schoolSelectAlreadyLoaded = false;
+function loadSchoolSelect() {
+    if (!schoolSelectAlreadyLoaded) {
+        schoolSelectAlreadyLoaded = true;
+
+        let schools = [];
+        Array.from(schoolData).forEach(landkreis => {
+            schools = schools.concat(landkreis.Schulen);
+        });
+
+        console.log(schools)
+
+        let fuse = new Fuse(schools, {
+            keys: ["Id", "Name", "Ort"]
+        })
+
+        app.autocomplete.create({
+            inputEl: '#school-dropdown',
+            openIn: 'dropdown',
+            source: async (query, render) => {
+                let items = fuse.search(query, {limit: 10});
+                console.log(items)
+                items = items.map((item) => `${item.item["Id"]} - ${item.item["Name"]} - ${item.item["Ort"]}`);
+                render(items);
+            }
+        });
+    }
 }
 
 document.getElementById("openSettingsScreenButton").addEventListener("click", () => {
@@ -25,9 +62,9 @@ document.getElementById("openSettingsScreenButton").addEventListener("click", ()
 })
 
 app.tab.show('#instanceConfigTab');
-
-  // Event-Handling für das Umschalten zwischen Tabs
-  app.on('tabShow', function (tabEl) {
+// Event-Handling für das Umschalten zwischen Tabs
+app.on('tabShow', function (tabEl) {
     var tabId = tabEl.id;
     app.tab.show(tabId);
-  });
+});
+
