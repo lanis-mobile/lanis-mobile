88f8d2bc07298f5fb7d272b3f5dc097fe0307b62
Alessio Caputo <84250128+alessioC42@users.noreply.github.com>
2023-09-11 20:59:40 +0200

    auto login feature

--- app/src/script.js
+++ app/src/script.js
@@ -1,6 +1,6 @@
 import Fuse from 'fuse.js';
 import schoolData from './schools.json';
-import {SecureStorage} from '@aparajita/capacitor-secure-storage'
+import { SecureStorage } from '@aparajita/capacitor-secure-storage'
 import { CapacitorHttp } from '@capacitor/core';
 import { Browser } from '@capacitor/browser';
 
@@ -34,18 +34,16 @@ function closeSettingsScreen() {
 async function isLoggedIn() {
   const serverURL = (await SecureStorage.getItem("serverURL"));
   const sid = (await SecureStorage.getItem("sid"));
-  const schoolid = (await SecureStorage.getItem("schoolid"));
 
-  if (serverURL && sid && schoolid) {
+  if (serverURL && sid) {
     let response = await CapacitorHttp.get({
       url: `${serverURL}/api/isValidSession`,
       params: {
-        schoolid: schoolid,
         sid: sid
       },
       responseType: "text",
       connectTimeout: REQUEST_TIMEOUT
-    }).catch(error => {
+    }).catch(_error => {
       app.toast.create({ text: 'Error calling the server!' }).open();
       document.getElementById("loginInformationLabel").innerText = "keine Verbindung";
     });
@@ -63,17 +61,10 @@ async function isLoggedIn() {
   }
 }
 
-async function login() {
-  app.dialog.preloader("Logging in...");
+async function auth(serverURL, username, password, schoolid) {
+  app.dialog.preloader("authenticating...");
 
   try {
-    let username = document.getElementById("login-username").value;
-    let password = document.getElementById("login-password").value;
-    let schoolid_raw = document.getElementById("login-schoolid").value
-    let schoolid = schoolid_raw.match(/^(\d+)/)[0];
-    let serverURL = (document.getElementById("login-instance").value).match(/^(https?:\/\/[a-zA-Z0-9.-]+)(:\d+)?/)[0];
-
-
     CapacitorHttp.get({
       url: `${serverURL}/api/login`,
       params: {
@@ -87,32 +78,59 @@ async function login() {
       app.dialog.close();
       if (response.status == 200) {
         let sid = await response.data;
-        await SecureStorage.setItem("sid", sid );
-        await SecureStorage.setItem("serverURL", serverURL);
-        await SecureStorage.setItem("schoolid", schoolid);
-        await SecureStorage.setItem("username", username );
+        console.log(sid);
+        await SecureStorage.setItem("sid", sid);
         app.toast.create({ text: 'logged in!' }).open()
 
         console.log(await SecureStorage.getItem("sid"));
         closeSettingsScreen()
-        
         updatePlanView();
 
+        document.getElementById("loginInformationLabel").innerText = "eingeloggt";
+
       } else if (response.status == 500) {
-        app.toast.create({ text: 'login failed!' }).open()
+        app.toast.create({ text: 'login failed!' }).open();
+        document.getElementById("loginInformationLabel").innerText = "nicht eingeloggt";
       } else {
-        app.toast.create({ text: 'login failed: unknown error' }).open()
+        app.toast.create({ text: 'login failed: unknown error' }).open();
+        document.getElementById("loginInformationLabel").innerText = "nicht eingeloggt";
       }
     }).catch(error => {
       console.error(error);
       app.dialog.close();
-      app.toast.create({ text: 'Login Failed: unknown error' }).open()
+      app.toast.create({ text: 'Login Failed: unknown error' }).open();
+      document.getElementById("loginInformationLabel").innerText = "nicht eingeloggt";
     });
 
   } catch (err) {
     app.dialog.close();
-    app.toast.create({ text: 'Login Failed: unknown error' }).open()
+    app.toast.create({ text: 'Login Failed: unknown error' }).open();
+    document.getElementById("loginInformationLabel").innerText = "nicht eingeloggt";
   }
+}
+
+async function loginButton() {
+  let username = document.getElementById("login-username").value;
+  let password = document.getElementById("login-password").value;
+  let schoolid_raw = document.getElementById("login-schoolid").value
+  let schoolid = schoolid_raw.match(/^(\d+)/)[0];
+  let serverURL = (document.getElementById("login-instance").value).match(/^(https?:\/\/[a-zA-Z0-9.-]+)(:\d+)?/)[0];
+  let autologin = document.getElementById("login-autologin").checked;
+
+  if (autologin) {
+    await SecureStorage.setItem("password", password);
+    await SecureStorage.setItem("autologin", "true");
+  } else {
+    await SecureStorage.setItem("password", "");
+    await SecureStorage.setItem("autologin", "");
+  }
+
+  await SecureStorage.setItem("serverURL", serverURL);
+  await SecureStorage.setItem("schoolid_raw", schoolid_raw);
+  await SecureStorage.setItem("schoolid", schoolid);
+  await SecureStorage.setItem("username", username);
+
+  auth(serverURL, username, password, schoolid);
 }
 
 function createCardItem(data) {
@@ -228,7 +246,7 @@ async function loadSchoolSelect() {
 async function loadMemoryEntryOptions() {
   let serverURL = (await SecureStorage.getItem("serverURL"));
   if (!serverURL) {
-    serverURL="https://production.sphvertretungsplan.alessioc42.workers.dev";
+    serverURL = "https://production.sphvertretungsplan.alessioc42.workers.dev";
   }
 
   let username = (await SecureStorage.getItem("username"));
@@ -237,7 +255,7 @@ async function loadMemoryEntryOptions() {
 
   }
 
-  document.getElementById("login-schoolid").value = (await SecureStorage.getItem("schoolid"));
+  document.getElementById("login-schoolid").value = (await SecureStorage.getItem("schoolid_raw"));
   document.getElementById("login-username").value = username;
   document.getElementById("login-instance").value = serverURL;
   document.getElementById("login-password").value = "";
@@ -254,21 +272,21 @@ async function wipeStorageAndRestartApp() {
 }
 
 async function openInBrowser(url) {
-  await Browser.open({url: url});
+  await Browser.open({ url: url });
 }
 
 document.getElementById("openSettingsScreenButton").addEventListener("click", openSettingsScreen);
 document.getElementById("closeSettingsScreenButton").addEventListener("click", closeSettingsScreen);
-document.getElementById("loginButton").addEventListener("click", login);
+document.getElementById("loginButton").addEventListener("click", loginButton);
 document.getElementById("reloadPlanDataButton").addEventListener("click", updatePlanView);
 document.getElementById("resetAppButton").addEventListener("click", wipeStorageAndRestartApp);
 
 
 //Buttons on startpage
-document.getElementById("browserOpenBugreport").addEventListener("click", ()=>{openInBrowser("https://github.com/alessioC42/SPH-vertretungsplan/issues")});
-document.getElementById("browserOpenFeatureRequest").addEventListener("click", ()=>{openInBrowser("https://github.com/alessioC42/SPH-vertretungsplan/issues")});
-document.getElementById("browserOpenLatestrelease").addEventListener("click", ()=>{openInBrowser("https://github.com/alessioC42/SPH-vertretungsplan/releases/latest")});
-document.getElementById("browserOpenGitHubPage").addEventListener("click", ()=>{openInBrowser("https://github.com/alessioC42/SPH-vertretungsplan")});
+document.getElementById("browserOpenBugreport").addEventListener("click", () => { openInBrowser("https://github.com/alessioC42/SPH-vertretungsplan/issues") });
+document.getElementById("browserOpenFeatureRequest").addEventListener("click", () => { openInBrowser("https://github.com/alessioC42/SPH-vertretungsplan/issues") });
+document.getElementById("browserOpenLatestrelease").addEventListener("click", () => { openInBrowser("https://github.com/alessioC42/SPH-vertretungsplan/releases/latest") });
+document.getElementById("browserOpenGitHubPage").addEventListener("click", () => { openInBrowser("https://github.com/alessioC42/SPH-vertretungsplan") });
 
 
 app.tab.show('#instanceConfigTab');
@@ -280,12 +298,30 @@ app.on('tabShow', function (tabEl) {
 
 
 async function init() {
+  loadMemoryEntryOptions();
   let loggedIn = await isLoggedIn();
-
+  console.log(`user logged in: ${loggedIn}`)
   if (loggedIn) {
     updatePlanView();
   } else {
-    openSettingsScreen();
+    let autologin = await SecureStorage.getItem("autologin");
+    let serverURL = await SecureStorage.getItem("serverURL");
+    let password =  await SecureStorage.getItem("password");
+    let username =  await SecureStorage.getItem("username");
+    let schoolid =  await SecureStorage.getItem("schoolid");
+
+    if (autologin && serverURL && username && password && schoolid) {
+      auth(serverURL, username, password, schoolid).then(() => {
+        updatePlanView();
+      }).catch(error => {
+        console.log(error);
+        app.toast.create({ text: 'Login Failed' }).open();
+        openSettingsScreen();
+      });
+    } else {
+      openSettingsScreen();
+      app.toast.create({ text: 'Du musst dich mit deinem LANIS account Anmelden, um diese App zu verwenden!' }).open()
+    }
   }
 }
 
