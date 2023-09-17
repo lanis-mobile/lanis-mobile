d77660827d72095ac5318a04456ad6b3021e2f00
Alessio Caputo <84250128+alessioC42@users.noreply.github.com>
2023-09-17 13:52:27 +0200

    add push notification plugin

---
+++ app/src/notifications.js
@@ -0,0 +1,15 @@
+import {LocalNotifications} from '@capacitor/local-notifications';
+
+
+/**
+ * @returns {Promise<boolean>} permissions
+ */
+export async function getPermissions(prompt = false) {
+
+}
+
+export async function createNotificationsFromPlanData(data) {
+
+}
+
+export async function createNotification() {}
