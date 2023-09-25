759f9166bc02ce161ce17855e97637ece5515ec4
Alessio Caputo <84250128+alessioC42@users.noreply.github.com>
2023-09-25 18:20:44 +0200

    getVplan working

--- flutter/lib/client/client.dart
+++ flutter/lib/client/client.dart
@@ -1,20 +1,23 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:cookie_jar/cookie_jar.dart';
 import 'package:dio/dio.dart';
 import 'package:dio_cookie_manager/dio_cookie_manager.dart';
 import 'package:dart_date/dart_date.dart';
+import 'package:flutter/cupertino.dart';
 
 class SPHclient {
   String username = "";
   String password = "";
-  int    schoolID = 5182;
-  final  dio = Dio();
+  int schoolID = 5182;
+  final dio = Dio();
 
-  SPHclient(this.username, this.password, this.schoolID){
+  SPHclient(this.username, this.password, this.schoolID) {
     dio.interceptors.add(CookieManager(PersistCookieJar()));
     dio.options.followRedirects = false;
-    dio.options.validateStatus = (status) => status != null && (status == 200 || status == 302);
+    dio.options.validateStatus =
+        (status) => status != null && (status == 200 || status == 302);
   }
 
   void overwriteCredits(String username, String password, int schoolID) {
@@ -23,24 +26,28 @@ class SPHclient {
     this.schoolID = schoolID;
   }
 
-  void loadCreditsFromStorage() {
-
-  }
+  void loadCreditsFromStorage() {}
 
   Future<int> login() async {
-    dio.options.validateStatus = (status) => status != null && (status == 200 || status == 302);
+    dio.options.validateStatus =
+        (status) => status != null && (status == 200 || status == 302);
     try {
       if (username.isNotEmpty && password.isNotEmpty) {
-        final response1 = await dio.post("https://login.schulportal.hessen.de/?i=$schoolID", queryParameters: {
-          "user": '$schoolID.$username',
-          "user2": username,
-          "password": password
-        }, options: Options(contentType: "application/x-www-form-urlencoded"));
-        if (response1.headers.value(HttpHeaders.locationHeader) != null){
+        final response1 = await dio.post(
+            "https://login.schulportal.hessen.de/?i=$schoolID",
+            queryParameters: {
+              "user": '$schoolID.$username',
+              "user2": username,
+              "password": password
+            },
+            options: Options(contentType: "application/x-www-form-urlencoded"));
+        if (response1.headers.value(HttpHeaders.locationHeader) != null) {
           //credits are valid
-          final response2 = await dio.get("https://connect.schulportal.hessen.de");
+          final response2 =
+              await dio.get("https://connect.schulportal.hessen.de");
 
-          String location2 = response2.headers.value(HttpHeaders.locationHeader) ?? "";
+          String location2 =
+              response2.headers.value(HttpHeaders.locationHeader) ?? "";
           await dio.get(location2);
           return 0;
           //login successful
@@ -61,4 +68,26 @@ class SPHclient {
     }
   }
 
+  Future<dynamic> getVplan(String date) async {
+    try {
+      final response = await dio.post(
+          "https://start.schulportal.hessen.de/vertretungsplan.php",
+          queryParameters: {"tag": date, "ganzerPlan": "true"},
+          data: 'tag=$date&ganzerPlan=true',
+          options: Options(headers: {
+            "Accept": "*/*",
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-origin",
+          }, ));
+      return jsonDecode(response.toString());
+    } on SocketException {
+      return -3;
+      //network error
+    } catch (e) {
+      return -4;
+      //unknown error;
+    }
+  }
 }
