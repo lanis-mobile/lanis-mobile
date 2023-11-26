1c465873752c003e5b8e9e1ef82aca79b926e406
Alessio Caputo <84250128+alessioC42@users.noreply.github.com>
2023-11-26 18:16:01 +0100

    Fix Calendar by using other Calendar Library :D

--- app/lib/view/calendar/calendar.dart
+++ app/lib/view/calendar/calendar.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_event_calendar/flutter_event_calendar.dart';
+import 'package:table_calendar/table_calendar.dart';
 import 'package:intl/intl.dart';
 
 import '../../client/client.dart';
@@ -12,176 +12,271 @@ class CalendarAnsicht extends StatefulWidget {
 }
 
 class _CalendarAnsichtState extends State<CalendarAnsicht> {
-  List<Event> events = [];
+  late final ValueNotifier<List<Event>> _selectedEvents;
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  RangeSelectionMode _rangeSelectionMode = RangeSelectionMode
+      .toggledOff; // Can be toggled on/off by longpressing a date
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+  DateTime? _rangeStart;
+  DateTime? _rangeEnd;
+  List<Event> eventList = [];
+
+  @override
+  void dispose() {
+    _selectedEvents.dispose();
+    super.dispose();
+  }
 
   @override
   void initState() {
+    performEventsRequest();
     super.initState();
 
-    DateTime time = DateTime.now();
-    onChange(CalendarDateTime(
-        year: time.year,
-        month: time.month,
-        day: time.day,
-        calendarType: CalendarType.GREGORIAN));
-
-    //TODO Better fix for start showing not working
-    Future.delayed(const Duration(milliseconds: 500), () {setState(() {});});
+    _selectedDay = _focusedDay;
+    _selectedEvents = ValueNotifier(_getEventsForDay(_selectedDay!));
   }
 
-  void iterateOverDateRange(
-      String startDateStr, String endDateStr, Function(DateTime) callback) {
-    DateFormat dateFormat = DateFormat('yyyy-MM-dd HH:mm:ss');
-    DateTime startDate = dateFormat.parse(startDateStr);
-    DateTime endDate = dateFormat.parse(endDateStr);
+  void performEventsRequest() {
+    DateTime currentDate = DateTime.now();
+    DateTime sixMonthsAgo = currentDate.subtract(const Duration(days: 180));
+    DateTime oneYearLater = currentDate.add(const Duration(days: 365));
 
-    for (DateTime date = startDate;
-        date.isBefore(endDate) || date.isAtSameMomentAs(endDate);
-        date = date.add(const Duration(days: 1))) {
-      callback(date);
-    }
+    final formatter = DateFormat('yyyy-MM-dd');
+
+    client.getCalendar(formatter.format(sixMonthsAgo), formatter.format(oneYearLater)).then((calendar) {
+      List<Event> updatedEventList = [];
+
+      calendar.forEach((event) {
+        updatedEventList.add(Event(event["title"], event, parseDateString(event["Anfang"]), parseDateString(event["Ende"])));
+      });
+
+      // Set the state with the updated event list
+      setState(() {
+        eventList = updatedEventList;
+        _selectedEvents.value = _getEventsForDay(_selectedDay!);
+      });
+    });
   }
 
-  Future<void> onChange(CalendarDateTime dateTime) async {
-    /*
-    IMPORTANT: https://github.com/novaday-co/flutter_event_calendar/issues/42
-     */
 
-    debugPrint("Updating Month: ${dateTime.month}");
+  List<Event> _getEventsForDay(DateTime day) {
+    List<Event> validEvents = [];
 
-    int year = dateTime.year;
-    int month = dateTime.month - 1;
-    int day = dateTime.day;
+    for (var event in eventList) {
+      // Compare only the date part of startTime and endTime
+      bool isStartTimeOnDay = event.startTime.year == day.year &&
+          event.startTime.month == day.month &&
+          event.startTime.day == day.day;
 
-    if (month < 1) {
-      month += 12;
-      year--;
-    }
+      bool isEndTimeOnDay = event.endTime.year == day.year &&
+          event.endTime.month == day.month &&
+          event.endTime.day == day.day;
 
-    String startDateTime =
-        DateFormat('yyyy-MM-dd').format(DateTime(year, month, day));
-
-    year = dateTime.year;
-    month = dateTime.month + 2;
-
-    if (month > 12) {
-      month -= 12;
-      year++;
-    }
-
-    DateTime twoMonthsLater = DateTime(year, month, day);
-    String endDateTime = DateFormat('yyyy-MM-dd').format(twoMonthsLater);
-
-    debugPrint("$startDateTime - $endDateTime");
-
-    await loadEvents(startDateTime, endDateTime);
-  }
-
-  Future<void> loadEvents(String startDate, String endDate, {secondTry= false}) async {
-
-    final data = await client.getCalendar(startDate, endDate);
-
-    if (data is int) {
-      if (!secondTry) {
-        await client.loadFromStorage();
-        await client.login();
-        await loadEvents(startDate, endDate, secondTry: true);
+      if ((isStartTimeOnDay || event.startTime.isBefore(day)) &&
+          (isEndTimeOnDay || event.endTime.isAfter(day))) {
+        validEvents.add(event);
       }
-    } else {
-      events.clear();
+    }
 
-      final List<String> keysNotRender = [
-        "Anfang",
-        "Ende",
-        "title",
-        "Institution",
-        "Id",
-        "FremdUID",
-        "LetzteAenderung",
-        "Verantwortlich",
-        "category",
-        "start",
-        "end"
-      ];
+    return validEvents;
+  }
 
-      for (final event in data) {
-        List<Widget> cardBody = [];
+  List<Event> _getEventsForRange(DateTime start, DateTime end) {
+    // Implementation example
+    final days = daysInRange(start, end);
 
-        String description = "";
-        event.forEach((key, value) {
-          if ((!keysNotRender.contains(key) && value != null && value != "")) {
-            if (key != "description") {
-              cardBody.add(Padding(
-                  padding: const EdgeInsets.only(right: 30, left: 30),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text("${key.toString()}:"),
-                      Text(value.toString())
-                    ],
-                  )));
-            } else {
-              description = value;
-            }
-          }
-        });
+    return [
+      for (final d in days) ..._getEventsForDay(d),
+    ];
+  }
 
-        if (description != "") {
-          cardBody.add(Padding(
-              padding:
-              const EdgeInsets.only(right: 30, left: 30, top: 5, bottom: 5),
-              child: Text(description)));
-        }
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    if (!isSameDay(_selectedDay, selectedDay)) {
+      setState(() {
+        _selectedDay = selectedDay;
+        _focusedDay = focusedDay;
+        _rangeStart = null; // Important to clean those
+        _rangeEnd = null;
+        _rangeSelectionMode = RangeSelectionMode.toggledOff;
+      });
 
-        cardBody.add(Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [Text(event["Anfang"]), Text(event["Ende"])],
-        ));
+      _selectedEvents.value = _getEventsForDay(selectedDay);
+    }
+  }
 
-        final card = Card(
-            child: ListTile(
-                title: Text(event["title"]),
-                subtitle: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Column(
-                      children: cardBody,
-                    )
-                  ],
-                )));
+  void _onRangeSelected(DateTime? start, DateTime? end, DateTime focusedDay) {
+    setState(() {
+      _selectedDay = null;
+      _focusedDay = focusedDay;
+      _rangeStart = start;
+      _rangeEnd = end;
+      _rangeSelectionMode = RangeSelectionMode.toggledOn;
+    });
 
-        iterateOverDateRange(event["Anfang"], event["Ende"], (date) {
-          events.add(Event(
-            child: card,
-            dateTime: CalendarDateTime(
-                year: date.year,
-                month: date.month,
-                day: date.day,
-                calendarType: CalendarType.GREGORIAN),
-          ));
-        });
-      }
+    // `start` or `end` could be null
+    if (start != null && end != null) {
+      _selectedEvents.value = _getEventsForRange(start, end);
+    } else if (start != null) {
+      _selectedEvents.value = _getEventsForDay(start);
+    } else if (end != null) {
+      _selectedEvents.value = _getEventsForDay(end);
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: EventCalendar(
-        calendarType: CalendarType.GREGORIAN,
-        calendarOptions: CalendarOptions(
-          toggleViewType: true,
-          viewType: ViewType.DAILY,
+    return Column(
+      children: [
+        TableCalendar<Event>(
+          firstDay: DateTime.utc(2020),
+          lastDay: DateTime.utc(2030),
+          focusedDay: _focusedDay,
+          selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+          rangeStartDay: _rangeStart,
+          rangeEndDay: _rangeEnd,
+          calendarFormat: _calendarFormat,
+          rangeSelectionMode: _rangeSelectionMode,
+          eventLoader: _getEventsForDay,
+          startingDayOfWeek: StartingDayOfWeek.monday,
+          calendarStyle: const CalendarStyle(
+            // Use `CalendarStyle` to customize the UI
+            outsideDaysVisible: false,
+          ),
+          onDaySelected: _onDaySelected,
+          onRangeSelected: _onRangeSelected,
+          onFormatChanged: (format) {
+            if (_calendarFormat != format) {
+              setState(() {
+                _calendarFormat = format;
+              });
+            }
+          },
+          onPageChanged: (focusedDay) {
+            _focusedDay = focusedDay;
+          },
         ),
-        calendarLanguage: 'en',
-        onMonthChanged: onChange,
-        onDateTimeReset: onChange,
-        onChangeDateTime: onChange,
-        eventOptions: EventOptions(
-          emptyText: "Keine Einträge",
+        const SizedBox(height: 8.0),
+        Expanded(
+          child: ValueListenableBuilder<List<Event>>(
+            valueListenable: _selectedEvents,
+            builder: (context, value, _) {
+              return ListView.builder(
+                itemCount: value.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: const EdgeInsets.symmetric(
+                      horizontal: 12.0,
+                      vertical: 4.0,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(),
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                    child: ListTile(
+                      onTap: () {
+
+                        List<Widget> cardBody = [];
+
+                        final List<String> keysNotRender = [
+                          "Anfang",
+                          "Ende",
+                          "title",
+                          "Institution",
+                          "Id",
+                          "FremdUID",
+                          "LetzteAenderung",
+                          "Verantwortlich",
+                          "category",
+                          "start",
+                          "end"
+                        ];
+
+                        String description = "";
+                        value[index].data.forEach((key, value) {
+                          if ((!keysNotRender.contains(key) && value != null && value != "")) {
+                            if (key != "description") {
+                              cardBody.add(Padding(
+                                  padding: const EdgeInsets.only(right: 1, left: 1),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text("${key.toString()}:"),
+                                      Text(value.toString())
+                                    ],
+                                  )));
+                            } else {
+                              description = value;
+                            }
+                          }
+                        });
+                        if (description != "") {
+                          cardBody.add(Text("\n$description"));
+                        }
+
+                        showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: Text(value[index].title),
+                                content: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: cardBody,
+                                ),
+                              );
+                            }
+                          );
+                      },
+                      title: Text('${value[index]}'),
+                    ),
+                  );
+                },
+              );
+            },
+          ),
         ),
-        events: events,
-      )
+      ],
     );
   }
+}
+
+
+class Event {
+  final String title;
+  final Map<String, dynamic> data;
+  final DateTime startTime;
+  final DateTime endTime;
+
+  const Event(this.title, this.data, this.startTime, this.endTime);
+
+  @override
+  String toString() => title;
+}
+
+List<DateTime> daysInRange(DateTime first, DateTime last) {
+  final dayCount = last.difference(first).inDays + 1;
+  return List.generate(
+    dayCount,
+        (index) => DateTime.utc(first.year, first.month, first.day + index),
+  );
+}
+
+DateTime parseDateString(String dateString) {
+  // Parse the date string
+  List<String> dateTimeParts = dateString.split(' ');
+  List<String> dateParts = dateTimeParts[0].split('-');
+  List<String> timeParts = dateTimeParts[1].split(':');
+
+  // Extract year, month, day, hour, minute, and second
+  int year = int.parse(dateParts[0]);
+  int month = int.parse(dateParts[1]);
+  int day = int.parse(dateParts[2]);
+  int hour = int.parse(timeParts[0]);
+  int minute = int.parse(timeParts[1]);
+  int second = int.parse(timeParts[2]);
+
+  // Create a DateTime object
+  DateTime dateTime = DateTime(year, month, day, hour, minute, second);
+
+  return dateTime;
 }
