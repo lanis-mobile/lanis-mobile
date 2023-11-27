c02a2233b9763fd6d60247e6d6989a47421a9894
Alessio Caputo <84250128+alessioC42@users.noreply.github.com>
2023-11-27 21:56:47 +0100

    Notifications settings screen logic

--- app/lib/view/settings/subsettings/notifications.dart
+++ app/lib/view/settings/subsettings/notifications.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:sph_plan/client/storage.dart';
 
 class NotificationsSettingsScreen extends StatefulWidget {
-  const NotificationsSettingsScreen({super.key});
+  const NotificationsSettingsScreen({Key? key}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() => _NotificationsSettingsScreenState();
@@ -10,6 +11,29 @@ class NotificationsSettingsScreen extends StatefulWidget {
 class _NotificationsSettingsScreenState extends State<NotificationsSettingsScreen> {
   bool _enableNotifications = true;
   int _notificationInterval = 15;
+
+  Future<void> applySettings() async {
+    await globalStorage.write(key: "settings-push-service-on", value: _enableNotifications.toString());
+    await globalStorage.write(key: "settings-push-service-interval", value: _notificationInterval.toString());
+  }
+
+  Future<void> loadSettingsVariables() async {
+    _enableNotifications = (await globalStorage.read(key: "settings-push-service-on") ?? "true") == "true";
+    _notificationInterval = int.parse(await globalStorage.read(key: "settings-push-service-interval") ?? "15");
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // Use await to ensure that loadSettingsVariables completes before continuing
+    loadSettingsVariables().then((_) {
+      setState(() {
+        // Set the state after loading the variables
+        _enableNotifications = _enableNotifications;
+        _notificationInterval = _notificationInterval;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,14 +69,26 @@ class _NotificationsSettingsScreenState extends State<NotificationsSettingsScree
           const ListTile(
             leading: Icon(Icons.info),
             title: Text("Information"),
-            subtitle: Text("Die Häufigkeit und der Zeitpunkt der Aktualisierung des Vertretungsplans hängen von verschiedenen Faktoren des Endgeräts ab. Im Akkusparmodus wird der Vertretungsplan beispielsweise oft nicht aktualisiert.\nEin Neustart der Anwendung ist erforderlich, um Änderungen zu übernehmen."),
+            subtitle: Text(
+                "Die Häufigkeit und der Zeitpunkt der Aktualisierung des Vertretungsplans hängen von verschiedenen Faktoren des Endgeräts ab. Im Akkusparmodus wird der Vertretungsplan beispielsweise oft nicht aktualisiert."),
           ),
           Padding(
             padding: const EdgeInsets.all(5),
             child: ElevatedButton(
-                onPressed: (){},
-                child: const Text("änderungen Speichern"),
+              onPressed: () {
+                applySettings();
 
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return const AlertDialog(
+                        title: Text("App Neustart erforderlich"),
+                        content: Text("Ein Neustart der Anwendung ist erforderlich, um Änderungen zu übernehmen."),
+                      );
+                    }
+                );
+              },
+              child: const Text("Änderungen Speichern"),
             ),
           )
         ],
---
+++ app/lib/view/settings/subsettings/supported_features.dart
@@ -0,0 +1,4 @@
+/*
+GET
+https://start.schulportal.hessen.de/startseite.php?a=ajax&f=apps
+ */
