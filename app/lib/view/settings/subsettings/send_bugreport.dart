dc52a8da9513304616b9830edc0727c06fa43136
Alessio Caputo <84250128+alessioC42@users.noreply.github.com>
2023-12-12 17:12:05 +0100

    Add form validation and network error bugfix

--- app/lib/view/settings/subsettings/send_bugreport.dart
+++ app/lib/view/settings/subsettings/send_bugreport.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:package_info_plus/package_info_plus.dart';
 
 
@@ -19,6 +20,7 @@ class _SendBugReportAnsichtState extends State<SendBugReportAnsicht> {
 
   TextEditingController bugDescriptionController = TextEditingController();
   TextEditingController contactInformationController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   bool sendMetadata = true;
 
   @override
@@ -40,127 +42,144 @@ class _SendBugReportAnsichtState extends State<SendBugReportAnsicht> {
       appBar: AppBar(
         title: const Text("Bugreport senden"),
       ),
-      body: ListView(
-        children: [
-          const ListTile(
-            title: Text("Danke, dass du aktiv zur Entwicklung der App beiträgst!"),
-            subtitle: Text("Es ist schwierig, eine APP für alle Schulen zu entwickeln."),
-          ),
-          Padding(
-            padding:
-                EdgeInsets.only(left: padding, right: padding, top: padding),
-            child:
-            TextField(
-              keyboardType: TextInputType.multiline,
-              maxLines: 8,
-              controller: bugDescriptionController,
-              decoration: const InputDecoration(
-                labelText: "Beschreibe den Bug",
-                alignLabelWithHint: true,
-                hintText: "Hier kannst du deinen Bug beschreiben...\n - Beschreibe den Bug... \n - Was hast du erwartet?\n - Was ist passiert?\n - Könnte es sich um ein Schulspezifisches Problem handeln?",
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          children: [
+            const ListTile(
+              title: Text("Danke, dass du aktiv zur Entwicklung der App beiträgst!"),
+              subtitle: Text("Es ist schwierig, eine APP für alle Schulen zu entwickeln."),
+            ),
+            Padding(
+              padding:
+              EdgeInsets.only(left: padding, right: padding, top: padding),
+              child:
+              TextFormField(
+                keyboardType: TextInputType.multiline,
+                maxLines: 8,
+                controller: bugDescriptionController,
+                decoration: const InputDecoration(
+                  labelText: "Beschreibe den Bug",
+                  alignLabelWithHint: true,
+                  hintText: "Hier kannst du deinen Bug beschreiben...\n - Beschreibe den Bug... \n - Was hast du erwartet?\n - Was ist passiert?\n - Könnte es sich um ein Schulspezifisches Problem handeln?",
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Bitte beschreibe den Bug.';
+                  }
+                  return null;
+                },
               ),
             ),
-          ),
-          Padding(
-            padding:
-            EdgeInsets.only(left: padding, right: padding, top: padding),
-            child:
-            TextField(
-              keyboardType: TextInputType.multiline,
-              maxLines: 4,
-              controller: contactInformationController,
-              decoration: const InputDecoration(
-                labelText: "Wie können wir dich erreichen?",
-                alignLabelWithHint: true,
-                hintText: "Vielleicht haben die Entwickler Fragen zu den Bugs. Eine Kontaktmöglichkeit wäre sehr hilfreich! \nz.B. Email oder Telefon",
+            Padding(
+              padding:
+              EdgeInsets.only(left: padding, right: padding, top: padding),
+              child:
+              TextFormField(
+                keyboardType: TextInputType.multiline,
+                maxLines: 4,
+                controller: contactInformationController,
+                decoration: const InputDecoration(
+                  labelText: "Wie können wir dich erreichen?",
+                  alignLabelWithHint: true,
+                  hintText: "Vielleicht haben die Entwickler Fragen zu den Bugs. Eine Kontaktmöglichkeit wäre sehr hilfreich! \nz.B. Email oder Telefon",
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Bitte gib Kontaktinformationen an.';
+                  }
+                  return null;
+                },
               ),
             ),
-          ),
-          SwitchListTile(
-            title: const Text("Metadaten Senden"),
-            subtitle: const Text("Dies beinhaltet in der Regel sensible Informationen über deine Schule und deine Kurse. Eventuell auch Informationen über dich als Person. In den meisten Fällen ist diese Information notwendig, um den Fehler reproduzieren zu können. Dein Passwort wird nicht mit den Entwicklern geteilt."),
-            value: sendMetadata,
-            onChanged: (state) {
-              setState(() {
-                sendMetadata = state;
-              });
-            }
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: padding, right: padding, top: padding),
-            child: ElevatedButton(
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context_) {
-                    return AlertDialog(
-                      title: const Text('Wirklich senden?'),
-                      content: const Text(
-                        'Wenn du auf "OK" klickst, werden deine Informationen an die Entwickler gesendet. Diese Aktion kann nicht rückgängig gemacht werden.',
-                      ),
-                      actions: <Widget>[
-                        TextButton(
-                          onPressed: () {
-                            Navigator.pop(context_, 'Cancel');
-                          },
-                          child: const Text('Cancel'),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.pop(context_, 'OK');
-                            showSendingDialog(context);
+            SwitchListTile(
+                title: const Text("Metadaten Senden"),
+                subtitle: const Text("Dies beinhaltet in der Regel sensible Informationen über deine Schule und deine Kurse. Eventuell auch Informationen über dich als Person. In den meisten Fällen ist diese Information notwendig, um den Fehler reproduzieren zu können. Dein Passwort wird nicht mit den Entwicklern geteilt."),
+                value: sendMetadata,
+                onChanged: (state) {
+                  setState(() {
+                    sendMetadata = state;
+                  });
+                }
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: padding, right: padding, top: padding),
+              child: ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context_) {
+                        return AlertDialog(
+                          title: const Text('Wirklich senden?'),
+                          content: const Text(
+                            'Wenn du auf "OK" klickst, werden deine Informationen an die Entwickler gesendet. Diese Aktion kann nicht rückgängig gemacht werden.',
+                          ),
+                          actions: <Widget>[
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pop(context_, 'Cancel');
+                              },
+                              child: const Text('Cancel'),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pop(context_, 'OK');
+                                showSendingDialog(context);
 
-                            sendToServer(
-                              bugDescriptionController.text,
-                              contactInformationController.text,
-                              sendMetadata,
-                            ).then((result) {
-                              Navigator.pop(context);
-                              if (result == 0) {
-                                showDialog(context: context, builder: (context) => AlertDialog(
-                                  title: const Text("Erfolg!"),
-                                  content: const Text("Der Fehlerbericht wurde Gesendet."),
-                                  actions: [
-                                    TextButton(
-                                      onPressed: () => Navigator.pop(context, "OK"),
-                                      child: const Text('OK'),
-                                    ),
-                                  ],
-                                ));
-                                clearInputs();
-                              } else {
-                                showDialog(context: context, builder: (context) => AlertDialog(
-                                  title: const Text("Unbekannter Fehler!"),
-                                  content: const Text("Der Fehlerbericht wurde nicht Gesendet."),
-                                  actions: [
-                                    TextButton(
-                                      onPressed: () => Navigator.pop(context, "OK"),
-                                      child: const Text('OK'),
-                                    ),
-                                  ],
-                                ));
-                              }
-                            }).catchError((error) {
-                              Navigator.pop(context);
-                              debugPrint('Error sending bug report: $error');
-                            });
-                          },
-                          child: const Text('OK'),
-                        ),
-                      ],
+                                sendToServer(
+                                  bugDescriptionController.text,
+                                  contactInformationController.text,
+                                  sendMetadata,
+                                ).then((result) {
+                                  Navigator.pop(context);
+                                  if (result == 0) {
+                                    showDialog(context: context, builder: (context) => AlertDialog(
+                                      title: const Text("Erfolg!"),
+                                      content: const Text("Der Fehlerbericht wurde Gesendet."),
+                                      actions: [
+                                        TextButton(
+                                          onPressed: () => Navigator.pop(context, "OK"),
+                                          child: const Text('OK'),
+                                        ),
+                                      ],
+                                    ));
+                                    clearInputs();
+                                  } else {
+                                    showDialog(context: context, builder: (context) => AlertDialog(
+                                      title: const Text("Fehler!"),
+                                      content: const Text("Der Fehlerbericht wurde nicht Gesendet."),
+                                      actions: [
+                                        TextButton(
+                                          onPressed: () => Navigator.pop(context, "OK"),
+                                          child: const Text('OK'),
+                                        ),
+                                      ],
+                                    ));
+                                  }
+                                }).catchError((error) {
+                                  Navigator.pop(context);
+                                  debugPrint('Error sending bug report: $error');
+                                });
+                              },
+                              child: const Text('OK'),
+                            ),
+                          ],
+                        );
+                      },
                     );
-                  },
-                );
-              },
-              child: const Text("Bugreport senden."),
+                  }
+                },
+                child: const Text("Bugreport senden."),
+              ),
             ),
-          ),
-          const ListTile(
-            trailing: Icon(Icons.info),
-            title: Text("Wie gehen wir mit deinen Daten um?"),
-            subtitle: Text("Sobald wir die Ursache des Fehlers gefunden haben oder den Fehler reproduzieren können, werden deine Daten umgehend gelöscht. Eventuell werden die Daten in einem gesicherten Rahmen mit anderen Entwicklern ausgetauscht."),
-          ),
-        ],
+            const ListTile(
+              trailing: Icon(Icons.info),
+              title: Text("Wie gehen wir mit deinen Daten um?"),
+              subtitle: Text("Sobald wir die Ursache des Fehlers gefunden haben oder den Fehler reproduzieren können, werden deine Daten umgehend gelöscht. Eventuell werden die Daten in einem gesicherten Rahmen mit anderen Entwicklern ausgetauscht."),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -219,19 +238,24 @@ Future<int> sendToServer(String bugDescription, String contactInformation, bool 
     }
   }
 
-  final response = await client.dio.post(
-    apiEndpointLocation,
-    data: {
-      "username": "${client.schoolID}.${client.username}",
-      "report": bugDescription,
-      "contact_information": contactInformation,
-      "device_data": deviceData,
-    },
-  );
-
-  if (response.statusCode == 200) {
-    return 0;
-  } else {
+  try {
+    final response = await client.dio.post(
+      apiEndpointLocation,
+      data: {
+        "username": "${client.schoolID}.${client.username}",
+        "report": bugDescription,
+        "contact_information": contactInformation,
+        "device_data": deviceData,
+      },
+    );
+    if (response.statusCode == 200) {
+      return 0;
+    } else {
+      return -4;
+    }
+  } on (SocketException,) {
+    return -3;
+  } catch (e) {
     return -4;
   }
 }
