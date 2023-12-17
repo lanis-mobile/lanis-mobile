e1306e8fd290b06a3c5db6ec98cc3e5ce54d71dc
kurwjan <kurwjann@gmail.com>
2023-12-17 14:26:18 +0100

    Fix fast mode not loading vp.

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -220,6 +220,8 @@ class _HomePageState extends State<HomePage> {
       client.prepareFetchers();
 
       if (client.loadMode == "fast") {
+        await fetchFeature([Status.substitution, Status.errorSubstitution, client.substitutionsFetcher]);
+
         statusController.add(Status.finalize);
         setState(() {
           isLoading = false;
