c8a6e8b5e5551d8f085bd7a6f66061d074927aa4
CodeSpoof <nao.s_l_t_e_el@protonmail.com>
2024-01-17 13:12:08 +0100

    Stundenplan Parser

--- app/lib/client/client.dart
+++ app/lib/client/client.dart
@@ -18,6 +18,7 @@ import 'package:sph_plan/themes.dart';
 
 import '../shared/apps.dart';
 import '../shared/shared_functions.dart';
+import 'fach.dart';
 
 class SPHclient {
   final statusCodes = {
@@ -477,6 +478,44 @@ class SPHclient {
       recordError(e, stack);
       return -4;
       //unknown error;
+    }
+  }
+
+  Future<List<List<List<StdPlanFach>>>> getStundenplan() async {
+    final location = await dio.get("https://start.schulportal.hessen.de/stundenplan.php");
+    final response = await dio.get("https://start.schulportal.hessen.de/${location.headers["location"]![0]}");
+
+    var document = parse(response.data);
+    var stundenplanTableHead = document.querySelector("#own thead");
+
+    var sk = stundenplanTableHead!.querySelector("th")!.text.contains("Stunde");
+
+    var stundenplanTableBody = document.querySelector("#own tbody");
+
+    if (stundenplanTableBody != null) {
+      List<List<List<StdPlanFach>>> result = [];
+
+      for (var row in stundenplanTableBody.querySelectorAll("tr")) {
+        if (row.text.replaceAll(RegExp(r'[\s\n\r]'), "") == "") continue;
+        List<List<StdPlanFach>> timeslot = [];
+        for (var (index, day) in row.querySelectorAll("td").indexed) {
+          if (sk && index == 0) continue;
+          List<StdPlanFach> stunde = [];
+          for (var fach in day.querySelectorAll(".stunde")) {
+            var name = fach.querySelector("b")!.text.trim();
+            var raum = fach.nodes.map((node) => node.nodeType == 3 ? node.text!.trim() : "").join();
+            var lehrer = fach.querySelector("small")!.text.trim();
+            var badge = fach.querySelector(".badge")?.text.trim() ?? "";
+            var duration = int.parse(fach.parent!.attributes["rowspan"]!);
+            stunde.add(StdPlanFach(name, raum, lehrer, badge, duration));
+          }
+          timeslot.add(stunde);
+        }
+        result.add(timeslot);
+      }
+      return result;
+    } else {
+      return [];
     }
   }
 
