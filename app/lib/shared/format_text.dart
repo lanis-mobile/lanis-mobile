01bd317a777584e2507665483d74021dab37c9ae
CodeSpoof <nao.s_l_t_e_el@protonmail.com>
2024-01-20 00:44:24 +0100

    Removed redundant quantifiers

--- app/lib/shared/format_text.dart
+++ app/lib/shared/format_text.dart
@@ -48,7 +48,7 @@ class FormattedText extends StatelessWidget {
           endTag: "</sub>"
       ),
       FormatPattern(
-          regExp: RegExp(r"_(\d){1,1}(?:(?!\n)\s)?"),
+          regExp: RegExp(r"_(\d)(?:(?!\n)\s)?"),
           startTag: "<sub>",
           endTag: "</sub>"
       ),
@@ -58,7 +58,7 @@ class FormattedText extends StatelessWidget {
           endTag: "</sup>"
       ),
       FormatPattern(
-          regExp: RegExp(r"\^(\d){1,1}(?:(?!\n)\s)?"),
+          regExp: RegExp(r"\^(\d)(?:(?!\n)\s)?"),
           startTag: "<sup>",
           endTag: "</sup>"
       ),
