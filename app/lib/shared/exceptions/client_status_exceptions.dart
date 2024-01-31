7b4c91a4a9e020ba73baac88a0e8026379647c83
CodeSpoof <nao.s_l_t_e_el@protonmail.com>
2024-01-31 21:14:09 +0100

    additions while converting

--- app/lib/shared/exceptions/client_status_exceptions.dart
+++ app/lib/shared/exceptions/client_status_exceptions.dart
@@ -1,44 +1,61 @@
-class WrongCredentialsException implements Exception {
+interface class LanisException implements Exception {
+  late String cause;
+  LanisException(this.cause);
+  LanisException.fromCode(dynamic data) {
+    cause = data.toString();
+  }
+}
+
+class WrongCredentialsException implements LanisException {
+  @override
   String cause;
   WrongCredentialsException([this.cause = "Falsche Anmeldedaten"]);
 }
 
-class CredentialsIncompleteException implements Exception {
+class CredentialsIncompleteException implements LanisException {
+  @override
   String cause;
-  CredentialsIncompleteException([this.cause = "Nicht alle Anmeldedaten angegeben"])
+  CredentialsIncompleteException([this.cause = "Nicht alle Anmeldedaten angegeben"]);
 }
 
-class NetworkException implements Exception {
+class NetworkException implements LanisException {
+  @override
   String cause;
   NetworkException([this.cause = "Netzwerkfehler"]);
 }
 
-class LoggedOffOrUnknownException implements Exception {
+class LoggedOffOrUnknownException implements LanisException {
+  @override
   String cause;
   LoggedOffOrUnknownException([this.cause = "Unbekannter Fehler! Bist du eingeloggt?"]);
 }
 
-class UnauthorizedException implements Exception {
+class UnauthorizedException implements LanisException {
+  @override
   String cause;
   UnauthorizedException([this.cause = "Keine Erlaubnis"]);
 }
 
-class EncryptionCheckFailedException implements Exception {
+class EncryptionCheckFailedException implements LanisException {
+  @override
   String cause;
   EncryptionCheckFailedException([this.cause = "Verschlüsselungsüberprüfung fehlgeschlagen"]);
 }
 
-class UnsaltedOrUnknownException implements Exception {
+class UnsaltedOrUnknownException implements LanisException {
+  @override
   String cause;
   UnsaltedOrUnknownException([this.cause = "Unbekannter Fehler! Antwort war nicht salted."]);
 }
 
-class NotSupportedException implements Exception {
+class NotSupportedException implements LanisException {
+  @override
   String cause;
   NotSupportedException([this.cause = "Nicht unterstützt!"]);
 }
 
-class NoConnectionException implements Exception {
+class NoConnectionException implements LanisException {
+  @override
   String cause;
   NoConnectionException([this.cause = "Kein Internet."]);
 }
