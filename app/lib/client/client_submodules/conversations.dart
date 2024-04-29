3eeb0131aee4351e8318e4595e2e37dbbe9e02d9
kurwjan <kurwjann@gmail.com>
2024-04-29 23:31:08 +0200

    Port of branch `post_conversations`
    + May not follow project "guidelines"

--- app/lib/client/client_submodules/conversations.dart
+++ app/lib/client/client_submodules/conversations.dart
@@ -7,6 +7,7 @@ import 'package:flutter/foundation.dart';
 
 import '../../shared/apps.dart';
 import '../../shared/exceptions/client_status_exceptions.dart';
+import '../../shared/types/conversations.dart';
 import '../client.dart';
 import '../connection_checker.dart';
 import '../cryptor.dart';
@@ -107,5 +108,152 @@ class ConversationsParser {
     } on (SocketException, DioException) {
       throw NetworkException();
     }
+  }
+
+  /// Replies to an already existing conversation.
+  ///
+  /// [headId] is the **"Uniquid"** of the head message.
+  ///
+  /// [groupOnly] and [privateAnswerOnly] are only checked for existence and then ignored.
+  ///
+  /// [sender] is the **"Sender"** of the head message, you can also use **"all"**.
+  ///
+  /// [message] supports Lanis-styled text.
+  ///
+  /// If successful, it returns `true`.
+  Future<dynamic> replyToConversation(String headId, int sender, bool groupOnly, bool privateAnswerOnly, String message) async {
+    final Map replyData = {
+      "to": sender.toString(),
+      "groupOnly": groupOnly,
+      "privateAnswerOnly": privateAnswerOnly,
+      "message": message,
+      "replyToMsg": headId,
+    };
+
+    String encrypted = client.cryptor.encryptString(json.encode(replyData));
+
+    final response = await dio.post("https://start.schulportal.hessen.de/nachrichten.php",
+        data: {
+          "a": "reply",
+          "c": encrypted,
+        },
+        options: Options(
+          headers: {
+            "Accept": "*/*",
+            "Content-Type":
+            "application/x-www-form-urlencoded; charset=UTF-8",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-origin",
+            "X-Requested-With": "XMLHttpRequest",
+          },
+        )
+    );
+
+    return json.decode(response.data)["back"]; // "back" should be bool, id is Uniquid
+  }
+
+  /// Creates a new conversation.
+  ///
+  /// [receivers] are Strings of Ids which you can get with `Unimplemented`.
+  /// Possible receivers are _(from the point of a student)_:
+  ///   - Students from the same _Lerngruppe_.
+  ///   - All teachers
+  ///
+  /// [type] is one of the following types:
+  ///   - `noAnswerAllowed`
+  ///     - **Hinweis**: No answers.
+  ///   - `privateAnswerOnly`
+  ///     - **Mitteilung**: Answers only to sender.
+  ///   - `groupOnly`
+  ///     - **Gruppenchat**: Answers to everyone.
+  ///   - `openChat`
+  ///     - **Offener Chat**: Private messages among themselves possible.
+  ///
+  ///  [text] also supports Lanis-styled text.
+  ///
+  ///  If successful, it returns true.
+  Future<dynamic> createConversation(List<String> receivers, String type, String subject, String text) async {
+    final List<Map<String, String>> createData = [
+      {
+        "name": "subject",
+        "value": subject
+      },
+      {
+        "name": "text",
+        "value": text
+      }
+    ];
+
+    for (final String receiver in receivers) {
+      createData.add({
+        "name": "to[]",
+        "value": receiver
+      });
+    }
+
+    String encrypted = client.cryptor.encryptString(json.encode(createData));
+
+    final response = await dio.post("https://start.schulportal.hessen.de/nachrichten.php",
+        data: {
+          "a": "newmessage",
+          "c": encrypted,
+        },
+        options: Options(
+          headers: {
+            "Accept": "*/*",
+            "Content-Type":
+            "application/x-www-form-urlencoded; charset=UTF-8",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-origin",
+            "X-Requested-With": "XMLHttpRequest",
+          },
+        )
+    );
+
+    return json.decode(response.data)["back"]; // "back" should be bool, id is Uniquid
+  }
+
+  /// Searches for teacher using at least 2 chars.
+  ///
+  /// Returns false if no one was found or a error happened.
+  /// On success, returns list of `SearchEntry`.
+  Future<dynamic> searchTeacher(String name) async {
+    if (name.length < 2) {
+      return false;
+    }
+
+    final response = await dio.get("https://start.schulportal.hessen.de/nachrichten.php",
+        queryParameters: {
+          "a": "searchRecipt",
+          "q": name
+        },
+        options: Options(
+          headers: {
+            "Accept": "*/*",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "none",
+          },
+        )
+    );
+
+    final data = json.decode(response.data);
+
+    if (data["items"] != null && data["items"].isNotEmpty) {
+      final List<SearchEntry> teacherEntries = [];
+
+      for (final teacher in data["items"]) {
+        teacherEntries.add(SearchEntry(
+            id: teacher["id"]!,
+            name: teacher["text"]!
+        ));
+      }
+
+      return teacherEntries;
+    }
+
+    return false;
   }
 }
---
+++ app/lib/shared/types/conversations.dart
@@ -0,0 +1,6 @@
+class SearchEntry {
+  final String id;
+  final String name;
+
+  SearchEntry({required this.id, required this.name});
+}
