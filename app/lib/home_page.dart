5df9bb19ba78d139f4c01600ab6f73bd3421c197
Alessio Caputo <84250128+alessioC42@users.noreply.github.com>
2024-06-02 20:46:40 +0200

    Prepare substitutions UI for static data

--- app/lib/home_page.dart
+++ app/lib/home_page.dart
@@ -14,7 +14,7 @@ import 'package:sph_plan/view/data_storage/data_storage.dart';
 import 'package:sph_plan/view/mein_unterricht/mein_unterricht.dart';
 import 'package:sph_plan/view/settings/settings.dart';
 import 'package:sph_plan/view/timetable/timetable.dart';
-import 'package:sph_plan/view/vertretungsplan/vertretungsplan.dart';
+import 'package:sph_plan/view/substitutions/stream.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:sph_plan/client/connection_checker.dart';
 
@@ -62,7 +62,7 @@ class _HomePageState extends State<HomePage> {
         isSupported: client.doesSupportFeature(SPHAppEnum.vertretungsplan),
         enableBottomNavigation: true,
         enableDrawer: true,
-        body: const VertretungsplanAnsicht()),
+        body: const SubstitutionsView()),
     Destination(
         label: (context) => AppLocalizations.of(context)!.calendar,
         icon: const Icon(Icons.calendar_today),
---
+++ app/lib/view/substitutions/stream.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:sph_plan/client/fetcher.dart';
+import 'package:sph_plan/view/substitutions/view.dart';
+
+import '../../client/client.dart';
+
+/// The [StaticSubstitutionsView] utilizing the [SubstitutionsFetcher] to display the substitutions.
+class SubstitutionsView extends StatefulWidget {
+  const SubstitutionsView({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _SubstitutionsViewState();
+}
+
+class _SubstitutionsViewState extends State<SubstitutionsView>
+    with TickerProviderStateMixin {
+  final SubstitutionsFetcher substitutionsFetcher =
+      client.fetchers.substitutionsFetcher;
+
+  @override
+  void initState() {
+    super.initState();
+    substitutionsFetcher.fetchData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<FetcherResponse>(
+      stream: substitutionsFetcher.stream,
+      builder: (context, snapshot) {
+        if (snapshot.data?.status == FetcherStatus.error) {
+          return StaticSubstitutionsView(
+            lanisException: snapshot.data?.error,
+            fetcher: substitutionsFetcher,
+            refresh: substitutionsFetcher.fetchData,
+            loading: false,
+          );
+        } else if (snapshot.data?.status == FetcherStatus.fetching ||
+            snapshot.data == null) {
+          return StaticSubstitutionsView(
+            fetcher: substitutionsFetcher,
+            refresh: substitutionsFetcher.fetchData,
+            loading: true,
+          );
+        } else {
+          return StaticSubstitutionsView(
+            plan: snapshot.data?.content,
+            fetcher: substitutionsFetcher,
+            refresh: substitutionsFetcher.fetchData,
+            loading: false,
+          );
+        }
+      },
+    );
+  }
+}
