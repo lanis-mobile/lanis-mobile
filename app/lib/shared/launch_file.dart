e921a4540c64b0d74d8d41a619bc0c7d05d2eadc
Alessio Caputo <84250128+alessioC42@users.noreply.github.com>
2024-09-06 20:46:36 +0200

    Fix: indicate user that no app to open file exists

--- app/lib/shared/launch_file.dart
+++ app/lib/shared/launch_file.dart
@@ -4,6 +4,8 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import '../client/client.dart';
+import '../client/logger.dart';
+
 
 void launchFile(BuildContext context, String url, String filename,
     String? filesize, Function callback) {
@@ -20,7 +22,7 @@ void launchFile(BuildContext context, String url, String filename,
         );
       });
 
-  client.downloadFile(url, filename).then((filepath) {
+  client.downloadFile(url, filename).then((filepath) async {
     Navigator.of(context).pop();
 
     if (filepath == "") {
@@ -53,7 +55,27 @@ void launchFile(BuildContext context, String url, String filename,
                 ],
               ));
     } else {
-      OpenFile.open(filepath);
+      final result = await OpenFile.open(filepath);
+      logger.i(result.message);
+      //sketchy, but "open_file" left us no other choice
+      if (result.message.contains("No APP found to open this file")) {
+        showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+                  title: Text("${AppLocalizations.of(context)!.error}!"),
+                  icon: const Icon(Icons.error),
+                  content: Text(
+                      AppLocalizations.of(context)!.noAppToOpen),
+                  actions: [
+                    FilledButton(
+                      child: const Text('Ok'),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ],
+                ));
+      }
       callback(); // Call the callback function after the file is opened
     }
   });
