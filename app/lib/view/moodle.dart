7bb17caa1179d24cfa193af4e7ad9d3c38485d70
kurwjan <kurwjann@gmail.com>
2024-09-23 22:41:41 +0200

    Switch back to official package
    + Update 6.1.0 fixed an issue with proguard or something else

--- app/lib/view/moodle.dart
+++ app/lib/view/moodle.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:webview_inapp/flutter_inappwebview.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
