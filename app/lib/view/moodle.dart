59dfb02268b75e8a867d4e61d085ea1ebed94cad
kurwjan <kurwjann@gmail.com>
2024-12-11 18:26:08 +0100

    Add first getCookies() implementation

--- app/lib/view/moodle.dart
+++ app/lib/view/moodle.dart
@@ -1,12 +1,18 @@
+import 'dart:convert';
+
+import 'package:dio_cookie_manager/dio_cookie_manager.dart' as dio_plugin;
+import 'package:cookie_jar/cookie_jar.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'dart:io' as io;
+import 'dart:io' as dio_core;
 
 import '../core/sph/sph.dart';
 import '../utils/file_operations.dart';
+import '../utils/logger.dart';
 
 class MoodleWebView extends StatefulWidget {
   const MoodleWebView({super.key});
@@ -18,25 +24,25 @@ class MoodleWebView extends StatefulWidget {
 class _MoodleWebViewState extends State<MoodleWebView> {
   static CookieManager cookieManager = CookieManager.instance();
 
+  late final Future<void> getCookiesFuture;
+
   ValueNotifier<bool> canGoBack = ValueNotifier(false);
   ValueNotifier<bool> canGoForward = ValueNotifier(false);
   ValueNotifier<int> progressIndicator = ValueNotifier(0);
-  ValueNotifier<bool> hideWebView = ValueNotifier(false);
-  ValueNotifier<bool> loggedIn = ValueNotifier(false);
+  ValueNotifier<bool> hideWebView = ValueNotifier(true);
   ValueNotifier<String> currentPageTitle = ValueNotifier("");
 
 
   String? error;
   WebUri? errorUrl;
-  bool errorDuringLogin = false;
 
-  bool reachedLogin = false;
+  bool isLoggedIn = false;
 
   InAppWebViewController? webViewController;
   PullToRefreshController? pullToRefreshController;
 
-  io.Cookie translateCookie(Cookie cookie) {
-    var target = io.Cookie(cookie.name, cookie.value);
+  dio_core.Cookie translateCookie(Cookie cookie) {
+    var target = dio_core.Cookie(cookie.name, cookie.value);
     target.domain = cookie.domain;
     target.path = cookie.path;
     target.secure = cookie.isSecure!;
@@ -45,12 +51,65 @@ class _MoodleWebViewState extends State<MoodleWebView> {
     return target;
   }
 
+  Future<void> getCookies() async {
+    final dio = Dio(BaseOptions(validateStatus: (status) => status != null));
+    final jar = CookieJar();
+    dio.options.followRedirects = false;
+    dio.interceptors.add(dio_plugin.CookieManager(jar));
+
+    final lastSchoolCookie = dio_core.Cookie("schulportal_lastschool", "6091");
+    lastSchoolCookie.domain = ".hessen.de";
+    lastSchoolCookie.path = "/";
+    lastSchoolCookie.secure = true;
+
+    jar.saveFromResponse(Uri.parse("https://login.schulportal.hessen.de/"), [
+      lastSchoolCookie
+    ]);
+
+    final response_1 = await dio.head("https://mo6091.schulportal.hessen.de");
+    final location_1 = response_1.headers.value("location")!;
+    logger.i("1");
+    logger.d(response_1.headers);
+
+    final response_2 = await dio.head(location_1);
+    final location_2 = response_2.headers.value("location")!;
+    logger.i("2");
+    logger.d(response_2.headers);
+    logger.i(location_2);
+
+    final response_3 = await dio.head(location_2);
+    final cookies = await jar.loadForRequest(Uri.parse(location_2));
+    final sphSessionPDataCookie = cookies.firstWhere((cookie) => cookie.name == "SPH-Sessionpdata").value;
+    final url = jsonDecode(Uri.decodeFull(sphSessionPDataCookie))["_url"];
+    logger.i("3");
+    logger.d(response_3.headers);
+
+    logger.i(url);
+
+    final response_4 = await dio.post(
+        location_2,
+      data: {
+          "user": "6091.dacjan.gapinski",
+          "user2": "dacjan.gapinski",
+          "password": "La-schGam-ing37456",
+          "url": url
+      }
+    );
+    logger.i("4");
+    logger.d(response_4.headers);
+    logger.i(response_4.requestOptions.data);
+    final location_3 = response_3.headers.value("location")!;
+
+    dio.close();
+    jar.deleteAll();
+  }
+
   void refresh() {
     if (webViewController != null) {
-      if (errorDuringLogin) {
+      /*if (errorDuringLogin) {
         errorDuringLogin = false;
         loggedIn.value = false;
-      }
+      }*/
 
       if (error != null) {
         webViewController!.loadUrl(urlRequest: URLRequest(
@@ -72,6 +131,8 @@ class _MoodleWebViewState extends State<MoodleWebView> {
     super.initState();
     cookieManager.deleteAllCookies();
 
+    getCookiesFuture = getCookies();
+
     progressIndicator.value = 1;
   }
 
@@ -88,11 +149,6 @@ class _MoodleWebViewState extends State<MoodleWebView> {
     pullToRefreshController!.setColor(Theme.of(context).colorScheme.primary);
     pullToRefreshController!
         .setBackgroundColor(Theme.of(context).colorScheme.surfaceContainer);
-  }
-
-  /// replaces ' with \'
-  String _sanitizeCredentialsForJavaScript(String source) {
-    return source.replaceAll("'", "\\'");
   }
 
   @override
@@ -183,6 +239,16 @@ class _MoodleWebViewState extends State<MoodleWebView> {
                 ),
               ),
             ],
+            FutureBuilder(
+                future: getCookies(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return SizedBox();
+                  }
+
+                  return SizedBox();
+                }
+            ),
             ValueListenableBuilder(
                 valueListenable: hideWebView,
                 builder: (context, hide, _) {
@@ -225,7 +291,7 @@ class _MoodleWebViewState extends State<MoodleWebView> {
                             return NavigationActionPolicy.CANCEL;
                           }
 
-                          if (reachedLogin && uri.rawValue.contains('start.schulportal.hessen.de')) {
+                          if (/*reachedLogin && */uri.rawValue.contains('start.schulportal.hessen.de')) {
                             hideWebView.value = true;
                             Navigator.pop(context);
                             return NavigationActionPolicy.CANCEL;
@@ -262,7 +328,7 @@ class _MoodleWebViewState extends State<MoodleWebView> {
                           currentPageTitle.value = title ?? "";
                         },
                         onPageCommitVisible: (controller, uri) async {
-                          if (!loggedIn.value) {
+                          /*if (!loggedIn.value) {
                             if (!reachedLogin && uri!.rawValue.contains("singleSignOn") && !uri.rawValue.contains(sph!.account.schoolID.toString())) {
                               controller.loadUrl(
                                   urlRequest: URLRequest(
@@ -305,7 +371,7 @@ class _MoodleWebViewState extends State<MoodleWebView> {
                                   cookies
                               );
                             }
-                          }
+                          }*/
 
                           // Hack to enable pull to refresh in Moodle.
                           controller.evaluateJavascript(
@@ -335,10 +401,10 @@ class _MoodleWebViewState extends State<MoodleWebView> {
                             errorUrl = request.url;
                           });
 
-                          if (loggedIn.value == false) {
+                          /*if (loggedIn.value == false) {
                             errorDuringLogin = true;
                             loggedIn.value = true;
-                          }
+                          }*/
 
                           pullToRefreshController!.endRefreshing();
                           progressIndicator.value = 0;
@@ -355,7 +421,7 @@ class _MoodleWebViewState extends State<MoodleWebView> {
                     ),
                   );
                 }),
-            ValueListenableBuilder(
+            /*ValueListenableBuilder(
                 valueListenable: loggedIn,
                 builder: (context, logged, _) {
                   return Visibility(
@@ -396,81 +462,74 @@ class _MoodleWebViewState extends State<MoodleWebView> {
                         ),
                       )
                   );
-                }),
+                }),*/
           ],
         ),
-        bottomNavigationBar: ValueListenableBuilder(
-          valueListenable: loggedIn,
-          builder: (context, logged, _) {
-            return logged == true
-                ? Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ValueListenableBuilder(
-                    valueListenable: progressIndicator,
-                    builder: (context, progress, _) {
-                      return Visibility(
-                        visible: progress != 0,
-                        maintainSize: true,
-                        maintainState: true,
-                        maintainAnimation: true,
-                        child: LinearProgressIndicator(
-                          value: progress / 100,
-                        ),
-                      );
-                    }),
-                Row(
-                  children: [
-                    IconButton(
-                        onPressed: refresh,
-                        icon: const Icon(Icons.refresh)),
-                    IconButton(
-                        onPressed: () async {
-                          if (webViewController != null) {
-                            await Clipboard.setData(ClipboardData(
-                                text: (await webViewController!.getUrl())!
-                                    .rawValue));
-                          }
-                        },
-                        icon: const Icon(Icons.link)),
-                    Expanded(
-                      child: Center(
-                          child: ValueListenableBuilder(valueListenable: currentPageTitle, builder: (context, title, _) =>
-                              Text(title,
-                                style: Theme.of(context).textTheme.labelLarge,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                          )
-                      ),
+        bottomNavigationBar: isLoggedIn ? Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ValueListenableBuilder(
+                valueListenable: progressIndicator,
+                builder: (context, progress, _) {
+                  return Visibility(
+                    visible: progress != 0,
+                    maintainSize: true,
+                    maintainState: true,
+                    maintainAnimation: true,
+                    child: LinearProgressIndicator(
+                      value: progress / 100,
                     ),
-                    ValueListenableBuilder(
-                        valueListenable: canGoBack,
-                        builder: (context, can, _) {
-                          return IconButton(
-                              onPressed: can
-                                  ? () {
-                                webViewController?.goBack();
-                              }
-                                  : null,
-                              icon: const Icon(Icons.arrow_back));
-                        }),
-                    ValueListenableBuilder(
-                        valueListenable: canGoForward,
-                        builder: (context, can, _) {
-                          return IconButton(
-                              onPressed: can
-                                  ? () {
-                                webViewController?.goForward();
-                              }
-                                  : null,
-                              icon: const Icon(Icons.arrow_forward));
-                        }),
-                  ],
+                  );
+                }),
+            Row(
+              children: [
+                IconButton(
+                    onPressed: refresh,
+                    icon: const Icon(Icons.refresh)),
+                IconButton(
+                    onPressed: () async {
+                      if (webViewController != null) {
+                        await Clipboard.setData(ClipboardData(
+                            text: (await webViewController!.getUrl())!
+                                .rawValue));
+                      }
+                    },
+                    icon: const Icon(Icons.link)),
+                Expanded(
+                  child: Center(
+                      child: ValueListenableBuilder(valueListenable: currentPageTitle, builder: (context, title, _) =>
+                          Text(title,
+                            style: Theme.of(context).textTheme.labelLarge,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                      )
+                  ),
                 ),
+                ValueListenableBuilder(
+                    valueListenable: canGoBack,
+                    builder: (context, can, _) {
+                      return IconButton(
+                          onPressed: can
+                              ? () {
+                            webViewController?.goBack();
+                          }
+                              : null,
+                          icon: const Icon(Icons.arrow_back));
+                    }),
+                ValueListenableBuilder(
+                    valueListenable: canGoForward,
+                    builder: (context, can, _) {
+                      return IconButton(
+                          onPressed: can
+                              ? () {
+                            webViewController?.goForward();
+                          }
+                              : null,
+                          icon: const Icon(Icons.arrow_forward));
+                    }),
               ],
-            )
-                : const SizedBox.shrink();
-          },
-        ));
+            ),
+          ],
+        ): SizedBox.shrink());
   }
 }
