60a7cad02599fcad788cafaf1a89b50174ebbf85
Vito0912 <86927734+Vito0912@users.noreply.github.com>
2024-12-26 22:43:07 +0100

    added first course draft

--- app/lib/applets/study_groups/student/parser.dart
+++ app/lib/applets/study_groups/student/parser.dart
@@ -21,8 +21,34 @@ class StudyGroupsStudentParser extends AppletParser<StudentStudyGroups> {
 
     Document document = parse(response.data);
 
-    Element? courses = document.getElementById('kurse');
+    Element? courses = document.getElementById('LGs');
     Element? exams = document.getElementById('klausuren');
+
+    // Courses parse thead
+    List<String> courseHeaders = [];
+    courses!.querySelectorAll('thead tr th').forEach((element) {
+      courseHeaders.add(element.text.trim());
+    });
+
+    courses.querySelectorAll('tbody tr').forEach((element) {
+      List<String> courseData = [];
+      element.querySelectorAll('td').forEach((element) {
+        // If <br> tag is present see each text as its own element
+        String html = element.innerHtml;
+        if (html.contains('<br>')) {
+          List<String> split = html.split('<br>');
+          for (var element in split) {
+            if (element.trim().isNotEmpty) {
+              courseData.add(element.trim());
+            }
+          }
+        } else {
+          courseData.add(element.text.trim());
+        }
+      });
+    });
+
+    // Exams parse thead
 
     return StudentStudyGroups.fromJson(jsonDecode(response.data));
   }
