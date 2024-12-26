c6043076504ca12f5d5dbe166addbc7ef944c58b
Vito0912 <86927734+Vito0912@users.noreply.github.com>
2024-12-27 00:06:42 +0100

    first draft for displaying

--- app/lib/applets/study_groups/student/student_study_groups_view.dart
+++ app/lib/applets/study_groups/student/student_study_groups_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:sph_plan/applets/study_groups/definitions.dart';
 import 'package:sph_plan/core/sph/sph.dart';
 import 'package:sph_plan/widgets/combined_applet_builder.dart';
@@ -13,15 +14,48 @@ class StudentStudyGroupsView extends StatefulWidget {
 class _StudentStudyGroupsViewState extends State<StudentStudyGroupsView> {
   @override
   Widget build(BuildContext context) {
-    return CombinedAppletBuilder(
-        parser: sph!.parser.studyGroupsStudentParser,
-        phpUrl: studyGroupsDefinition.appletPhpUrl,
-        settingsDefaults: studyGroupsDefinition.settingsDefaults,
-        accountType: sph!.session.accountType,
-        builder:
-            (context, data, accountType, settings, updateSetting, refresh) {
-          //
-          return SizedBox();
-        });
+    return Scaffold(
+      body: CombinedAppletBuilder(
+          parser: sph!.parser.studyGroupsStudentParser,
+          phpUrl: studyGroupsDefinition.appletPhpUrl,
+          settingsDefaults: studyGroupsDefinition.settingsDefaults,
+          accountType: sph!.session.accountType,
+          builder:
+              (context, data, accountType, settings, updateSetting, refresh) {
+            //
+            return ListView.builder(
+              itemCount: data.length,
+              itemBuilder: (context, index) {
+                if (data[index].exams.isEmpty) return Container();
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(data[index].courseName),
+                          Text(data[index].teacher),
+                          Text(DateFormat('dd.MM.yy')
+                              .format(data[index].exams[0].date)),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(data[index].exams[0].type),
+                          data[index].exams[0].duration.isEmpty
+                              ? Text(data[index].exams[0].time)
+                              : Text(
+                                  '${data[index].exams[0].time} (${data[index].exams[0].duration})'),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
+          }),
+    );
   }
 }
