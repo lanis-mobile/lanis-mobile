4ba461df4bba89ea66b966092991f746cb8250a1
Vito0912 <86927734+Vito0912@users.noreply.github.com>
2024-12-27 12:14:16 +0100

    study groups design

--- app/lib/applets/study_groups/student/student_exams_view.dart
+++ app/lib/applets/study_groups/student/student_exams_view.dart
@@ -1,4 +1,6 @@
+import 'package:dart_date/dart_date.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:intl/intl.dart';
 import 'package:sph_plan/models/study_groups.dart';
 
@@ -8,53 +10,80 @@ class StudentExamsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    DateTime today = DateTime(2024, 11, 10);
+    bool todayMarkerShown = false;
     return ListView.builder(
       padding: const EdgeInsets.only(top: 42),
       itemCount: studyData.length,
       itemBuilder: (context, index) {
-        return Padding(
-          padding: const EdgeInsets.all(4.0),
-          child: Card(
-            child: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        bool showMarker = !todayMarkerShown &&
+            studyData[index].exam.date.isAfter(today.subDays(1));
+        int difference = studyData[index].exam.date.difference(today).inDays;
+        if (showMarker) todayMarkerShown = true;
+        return Column(
+          children: [
+            if (showMarker && difference != 0)
+              Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Row(
+                    spacing: 8,
                     children: [
-                      Text(studyData[index].courseName,
-                          style: Theme.of(context).textTheme.labelLarge),
+                      Text(DateFormat('dd.MM.yy').format(today)),
+                      Expanded(child: Divider()),
+                      // Show days until next exam
+                      Text(AppLocalizations.of(context)!
+                          .daysUntilNextExam(difference)),
+                    ],
+                  )),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Card(
+                color: showMarker && difference == 0
+                    ? Theme.of(context).colorScheme.secondaryContainer
+                    : null,
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
                       Row(
-                        spacing: 4,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Text(
-                              DateFormat('dd.MM.yy')
-                                  .format(studyData[index].exam.date),
+                          Text(studyData[index].courseName,
                               style: Theme.of(context).textTheme.labelLarge),
-                          Icon(Icons.calendar_today, size: 16),
+                          Row(
+                            spacing: 4,
+                            children: [
+                              Text(
+                                  DateFormat('dd.MM.yy')
+                                      .format(studyData[index].exam.date),
+                                  style:
+                                      Theme.of(context).textTheme.labelLarge),
+                              Icon(Icons.calendar_today, size: 16),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Wrap(
+                        alignment: WrapAlignment.spaceBetween,
+                        runAlignment: WrapAlignment.spaceBetween,
+                        crossAxisAlignment: WrapCrossAlignment.start,
+                        direction: Axis.horizontal,
+                        children: [
+                          Text(studyData[index].exam.type),
+                          Text(studyData[index].teacherKuerzel),
+                          studyData[index].exam.duration.isEmpty
+                              ? Text(studyData[index].exam.time)
+                              : Text(
+                                  '${studyData[index].exam.time} (${studyData[index].exam.duration})'),
                         ],
                       ),
                     ],
                   ),
-                  Wrap(
-                    alignment: WrapAlignment.spaceBetween,
-                    runAlignment: WrapAlignment.spaceBetween,
-                    crossAxisAlignment: WrapCrossAlignment.start,
-                    direction: Axis.horizontal,
-                    children: [
-                      Text(studyData[index].exam.type),
-                      Text(studyData[index].teacherKuerzel),
-                      studyData[index].exam.duration.isEmpty
-                          ? Text(studyData[index].exam.time)
-                          : Text(
-                              '${studyData[index].exam.time} (${studyData[index].exam.duration})'),
-                    ],
-                  ),
-                ],
+                ),
               ),
             ),
-          ),
+          ],
         );
       },
     );
