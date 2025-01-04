5cee05db215b517a7550a2e72c3089d77d12467d
Rajala1404 <115874847+Rajala1404@users.noreply.github.com>
2025-01-04 19:35:43 +0100

    use rounded download icon

--- app/lib/applets/calendar/calendar_view.dart
+++ app/lib/applets/calendar/calendar_view.dart
@@ -357,7 +357,7 @@ class _CalendarViewState extends State<CalendarView> {
         ),
         actions: [
           IconButton(
-            icon: const Icon(Icons.download_outlined),
+            icon: const Icon(Icons.download_rounded),
             onPressed: () {
               Navigator.push(
                 context,
