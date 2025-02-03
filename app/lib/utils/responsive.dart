7bf39f4ceb96623f40747e5c9de4d230612d056c
Kaan <kaanhd79@gmail.com>
2025-02-04 00:07:52 +0100

    update tablet detection logic to include wider screen sizes

--- app/lib/utils/responsive.dart
+++ app/lib/utils/responsive.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class Responsive {
   static bool isTablet(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return size.width > 600 && size.width < 1200;
+    return size.width > 600;
   }
 }
