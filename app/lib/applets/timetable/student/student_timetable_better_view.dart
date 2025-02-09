898bc7650873160fc1c4c4eef2fa474532ae2a31
Vito0912 <86927734+Vito0912@users.noreply.github.com>
2025-02-09 16:23:33 +0100

    support multiple courses per day

--- app/lib/applets/timetable/student/student_timetable_better_view.dart
+++ app/lib/applets/timetable/student/student_timetable_better_view.dart
@@ -7,6 +7,9 @@ import 'package:sph_plan/models/account_types.dart';
 import 'package:sph_plan/models/timetable.dart';
 import 'package:sph_plan/widgets/combined_applet_builder.dart';
 
+final double itemHeight = 60;
+final double headerHeight = 30;
+
 class StudentTimetableBetterView extends StatefulWidget {
   final Function? openDrawerCb;
   const StudentTimetableBetterView({super.key, this.openDrawerCb});
@@ -34,22 +37,6 @@ class _StudentTimetableBetterViewState
     final days = now.difference(firstDayOfYear).inDays;
     return ((days + firstDayOfYear.weekday - 1) / 7).ceil();
   }
-
-  BorderRadius _getBorderRadius(bool previous, bool next) {
-    if (previous && next) {
-      return BorderRadius.zero;
-    } else if (previous) {
-      return BorderRadius.only(
-          bottomRight: Radius.circular(8.0), bottomLeft: Radius.circular(8.0));
-    } else if (next) {
-      return BorderRadius.only(
-          topRight: Radius.circular(8.0), topLeft: Radius.circular(8.0));
-    }
-    return BorderRadius.all(Radius.circular(8.0));
-  }
-
-  final double itemHeight = 60;
-  final double headerHeight = 30;
 
   @override
   Widget build(BuildContext context) {
@@ -144,118 +131,14 @@ class _StudentTimetableBetterViewState
                                     textAlign: TextAlign.center,
                                   ),
                                 ),
-                                for (var (index, row) in data.hours!.indexed)
-                                  Builder(builder: (context) {
-                                    int subjectIndex =
-                                        selectedPlan[i].indexWhere((element) {
-                                      if (element.startTime <= row.startTime &&
-                                          element.endTime >= row.endTime) {
-                                        return true;
-                                      }
-                                      return false;
-                                    });
-                                    TimetableSubject? subject =
-                                        subjectIndex != -1
-                                            ? selectedPlan[i][subjectIndex]
-                                            : null;
-
-                                    bool connectedToNext = false;
-                                    bool connectedToPrevious = false;
-                                    if (subjectIndex != -1) {
-                                      if (index > 0) {
-                                        int subjectIndex = selectedPlan[i]
-                                            .indexWhere((element) {
-                                          if (element.startTime <=
-                                                  data.hours[index - 1]
-                                                      .startTime &&
-                                              element.endTime >=
-                                                  data.hours[index - 1]
-                                                      .endTime) {
-                                            return true;
-                                          }
-                                          return false;
-                                        });
-                                        if (subjectIndex != -1) {
-                                          connectedToPrevious = true;
-                                        }
-                                      }
-                                      if (index < data.hours.length - 1) {
-                                        int subjectIndex = selectedPlan[i]
-                                            .indexWhere((element) {
-                                          if (element.startTime <=
-                                                  data.hours[index + 1]
-                                                      .startTime &&
-                                              element.endTime >=
-                                                  data.hours[index + 1]
-                                                      .endTime) {
-                                            return true;
-                                          }
-                                          return false;
-                                        });
-                                        if (subjectIndex != -1) {
-                                          connectedToNext = true;
-                                        }
-                                      }
-                                    }
-
-                                    Color lessonColor = row.type ==
-                                            TimeTableRowType.lesson
-                                        ? subject != null
-                                            ? TimeTableHelper.getColorForLesson(
-                                                settings, subject)
-                                            : Theme.of(context)
-                                                .colorScheme
-                                                .surfaceContainer
-                                        : Theme.of(context)
-                                            .colorScheme
-                                            .surfaceContainer
-                                            .withOpacity(0.5);
-
-                                    TextStyle? textStyle = Theme.of(context)
-                                        .textTheme
-                                        .bodyMedium
-                                        ?.copyWith(
-                                            color:
-                                                lessonColor.computeLuminance() >
-                                                        0.5
-                                                    ? Colors.black
-                                                    : Colors.white);
-
-                                    return Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.stretch,
-                                      children: [
-                                        SizedBox(
-                                          height: connectedToPrevious ? 0 : 8.0,
-                                        ),
-                                        Container(
-                                            decoration: BoxDecoration(
-                                              color: lessonColor,
-                                              borderRadius: _getBorderRadius(
-                                                  connectedToPrevious,
-                                                  connectedToNext),
-                                            ),
-                                            height: itemHeight -
-                                                (row.type ==
-                                                        TimeTableRowType.lesson
-                                                    ? 0
-                                                    : 20) +
-                                                (connectedToPrevious ? 8 : 0),
-                                            child: row.type ==
-                                                        TimeTableRowType
-                                                            .lesson &&
-                                                    subject != null &&
-                                                    !connectedToPrevious
-                                                ? Column(
-                                                    children: [
-                                                      Text(subject.name!,
-                                                          style: textStyle)
-                                                    ],
-                                                  )
-                                                : SizedBox()),
-                                      ],
-                                    );
-                                  }),
+                                for (var (index, row) in data.hours.indexed)
+                                  LessonItem(
+                                      i: i,
+                                      index: index,
+                                      row: row,
+                                      selectedPlan: selectedPlan,
+                                      settings: settings,
+                                      data: data),
                               ],
                             ),
                           )
@@ -267,6 +150,148 @@ class _StudentTimetableBetterViewState
             ),
           );
         });
+  }
+}
+
+class LessonItem extends StatelessWidget {
+  final int index;
+  final int i;
+  final TimeTableRow row;
+  final List<TimetableDay> selectedPlan;
+  final Map<String, dynamic> settings;
+  final TimeTableData data;
+  const LessonItem(
+      {super.key,
+      required this.index,
+      required this.i,
+      required this.row,
+      required this.selectedPlan,
+      required this.settings,
+      required this.data});
+
+  BorderRadius _getBorderRadius(bool previous, bool next) {
+    if (previous && next) {
+      return BorderRadius.zero;
+    } else if (previous) {
+      return BorderRadius.only(
+          bottomRight: Radius.circular(8.0), bottomLeft: Radius.circular(8.0));
+    } else if (next) {
+      return BorderRadius.only(
+          topRight: Radius.circular(8.0), topLeft: Radius.circular(8.0));
+    }
+    return BorderRadius.all(Radius.circular(8.0));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Filter all lessons that cover the current row's time slot.
+    List<TimetableSubject> overlappingSubjects = selectedPlan[i]
+        .where((element) =>
+            element.startTime <= row.startTime &&
+            element.endTime >= row.endTime)
+        .toList();
+
+    // Check connection for visual continuity on the first lesson (if needed).
+    bool connectedToPrevious = false;
+    bool connectedToNext = false;
+    if (overlappingSubjects.isNotEmpty) {
+      if (index > 0) {
+        int prevIndex = selectedPlan[i].indexWhere((element) =>
+            element.startTime <= data.hours[index - 1].startTime &&
+            element.endTime >= data.hours[index - 1].endTime);
+        connectedToPrevious = prevIndex != -1;
+      }
+      if (index < data.hours.length - 1) {
+        int nextIndex = selectedPlan[i].indexWhere((element) =>
+            element.startTime <= data.hours[index + 1].startTime &&
+            element.endTime >= data.hours[index + 1].endTime);
+        connectedToNext = nextIndex != -1;
+      }
+    }
+
+    return Row(
+      children: overlappingSubjects.isNotEmpty
+          ? overlappingSubjects.map((subject) {
+              Color lessonColor = row.type == TimeTableRowType.lesson
+                  ? TimeTableHelper.getColorForLesson(settings, subject)
+                  : Theme.of(context)
+                      .colorScheme
+                      .surfaceContainer
+                      .withOpacity(0.5);
+              TextStyle? textStyle = Theme.of(context)
+                  .textTheme
+                  .bodyMedium
+                  ?.copyWith(
+                      color: lessonColor.computeLuminance() > 0.5
+                          ? Colors.black
+                          : Colors.white);
+              return Expanded(
+                child: Padding(
+                  padding: EdgeInsets.only(
+                    left: overlappingSubjects.first == subject ? 0 : 1.0,
+                    right: overlappingSubjects.last == subject ? 0 : 1.0,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      SizedBox(
+                        height: connectedToPrevious ? 0 : 8.0,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: lessonColor,
+                          borderRadius: _getBorderRadius(
+                              connectedToPrevious, connectedToNext),
+                        ),
+                        height: itemHeight -
+                            (row.type == TimeTableRowType.lesson ? 0 : 20) +
+                            (connectedToPrevious ? 8 : 0),
+                        child: row.type == TimeTableRowType.lesson &&
+                                !connectedToPrevious
+                            ? Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    subject.name!,
+                                    style: textStyle,
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ],
+                              )
+                            : const SizedBox(),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            }).toList()
+          : [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    SizedBox(
+                      height: connectedToPrevious ? 0 : 8.0,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Theme.of(context)
+                            .colorScheme
+                            .surfaceContainer
+                            .withOpacity(0.5),
+                        borderRadius: _getBorderRadius(
+                            connectedToPrevious, connectedToNext),
+                      ),
+                      height: itemHeight -
+                          (row.type == TimeTableRowType.lesson ? 0 : 20) +
+                          (connectedToPrevious ? 8 : 0),
+                      child: const SizedBox(),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+    );
   }
 }
 
