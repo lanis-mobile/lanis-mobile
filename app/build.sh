cb3b1537ce9099b484eecd110ae48bbddf995184
Alessio Caputo <84250128+alessioC42@users.noreply.github.com>
2025-03-16 13:23:44 +0100

    chore: update build script and dependencies for version bump

--- app/build.sh
+++ app/build.sh
@@ -17,3 +17,6 @@ mv build/app/outputs/bundle/release/app-release.aab artifacts/app-release.aab
 echo ">>> build apk"
 flutter build apk --dart-define=cronetHttpNoPlay=true
 mv build/app/outputs/flutter-apk/app-release.apk artifacts/app-release-selfsigned.apk
+
+# Kill left over gradle daemons
+pkill -f '.GradleDaemon.'
